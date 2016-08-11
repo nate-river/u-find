8b3e647bf7290a6bbd0becbd9a9880d193d99ae5
nate-river <mayanlong7@icloud.com>
2016-08-11 17:24:26 +0800

    ..

--- app.js
+++ app.js
@@ -1,185 +1,183 @@
 var express = require('express');
 var pinyin = require('pinyin');
-var mysql      = require('mysql');
+var mysql = require('mysql');
 var crypto = require('crypto');
 var cookieParser = require('cookie-parser');
 
 var app = express();
-
-app.use( express.static( __dirname + '/static/') );
+app.use(express.static(__dirname + '/static/'));
 app.use(cookieParser());
 
-// var check = function (req, res, next) {
-//   var url = req.originalUrl;
-//   if(url === '/app/login' || url === '/login' ){
-//
-//   }
-//   // if (url !== '/app/login' && !req.cookies.___uek___) {
-//   //   return res.redirect("/app/login");
-//   // }
-//   next();
-// };
-// app.use(check);
 
-app.get('/app/login', function (req, res) {
-  res.sendFile(__dirname+'/phone/m_login.html'  ) ;
+
+app.get('/app/login', function(req, res) {
+	res.sendFile(__dirname + '/phone/m_login.html');
 });
 
-app.get('/login', function (req, res) {
-  res.sendFile(__dirname+'/pc/login.html'  ) ;
+app.get('/login', function(req, res) {
+	res.sendFile(__dirname + '/pc/login.html');
 });
 
-app.get('/app/', function (req, res) {
-  res.sendFile(__dirname+'/phone/m_index.html'  ) ;
+app.get('/app/', function(req, res) {
+	if(res.cookies.__uek__){
+		res.sendFile(__dirname + '/phone/m_index.html');
+	}else{
+		res.redirect('/app/login');
+	}
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname+'/pc/index.html'  ) ;
+app.get('/', function(req, res) {
+	var account = req.cookies.__uek__;
+	var columns = ['authority'];
+	connection.query('SELECT ?? from user where phone = ?', [columns, account], function(err, rows, fields) {
+		if (err) throw err;
+		if ( rows[0].authority === 1 ){
+			res.sendFile(__dirname + '/pc/index.html');
+		}else{
+			res.redirct('/login');
+		}
+	});
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(3000, function() {
+	console.log('Example app listening on port 3000!');
 });
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  port     : '3306',
-  user     : 'root',
-  password : 'root',
-  database : 'uek',
-  // socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
+	host: 'localhost',
+	port: '3306',
+	user: 'root',
+	password: 'root',
+	database: 'uek',
+	// socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
 });
 
 
 ///////////////////////////////////////////////////////////////////////////////
-app.get('/checkUser', function (req, res) {
+app.get('/checkUser', function(req, res) {
 
-  var hash = crypto.createHash("md5");
-  hash.update(new Buffer(req.query.password, "binary"));
-  var encode = hash.digest('hex');
+	var hash = crypto.createHash("md5");
+	hash.update(new Buffer(req.query.password, "binary"));
+	var encode = hash.digest('hex');
 
-  connection.query('SELECT ?? FROM user where phone = ?',
-  ['password',req.query.account]
-  , function(err, result) {
-    if( result[0] && (result[0].password === encode) ){
-      res.json({
-        phone:req.query.account,
-        password:result[0].password,
-      });
-    }else{
-      res.json(false);
-    }
-  });
+	connection.query('SELECT ?? FROM user where phone = ?', ['password', req.query.account], function(err, result) {
+		if (result[0] && (result[0].password === encode)) {
+			res.jsonp({
+				phone: req.query.account,
+				password: result[0].password,
+			});
+		} else {
+			res.jsonp(false);
+		}
+	});
 });
 
-app.get('/resetPasswordByAccount',function(req,res){
+app.get('/resetPasswordByAccount', function(req, res) {
 
-  var hash = crypto.createHash("md5");
-  hash.update(new Buffer("123456", "binary"));
-  var password = hash.digest('hex');
+	var hash = crypto.createHash("md5");
+	hash.update(new Buffer("123456", "binary"));
+	var password = hash.digest('hex');
 
-  connection.query( 'UPDATE user SET password = ?  WHERE phone = ?',
-  [password,req.query.account],function(err,result){
-    if (err){
-      res.json(false);
-    }else{
-      res.json(true);
-    }
-  });
+	connection.query('UPDATE user SET password = ?  WHERE phone = ?', [password, req.query.account], function(err, result) {
+		if (err) {
+			res.jsonp(false);
+		} else {
+			res.jsonp(true);
+		}
+	});
 });
 
-app.get('/setPassword',function(req,res){
-  var hash = crypto.createHash("md5");
-  hash.update(new Buffer(req.query.password, "binary"));
-  var password = hash.digest('hex');
-  connection.query( 'UPDATE user SET password = ?  WHERE phone = ?',
-  [password,req.query.account],function(err,result){
-    if (err){
-      res.json(false);
-    }else{
-      res.json(true);
-    }
-  });
+app.get('/setPassword', function(req, res) {
+	var hash = crypto.createHash("md5");
+	hash.update(new Buffer(req.query.password, "binary"));
+	var password = hash.digest('hex');
+	connection.query('UPDATE user SET password = ?  WHERE phone = ?', [password, req.query.account], function(err, result) {
+		if (err) {
+			res.jsonp(false);
+		} else {
+			res.jsonp(true);
+		}
+	});
 });
 
 ////////////////////////////////////////////////////////////////////////////////
 
-app.get('/addUser', function (req, res) {
-  var date = new Date();
+app.get('/addUser', function(req, res) {
+	var date = new Date();
 
-  var hash = crypto.createHash("md5");
-  hash.update(new Buffer("123456", "binary"));
-  var encode = hash.digest('hex');
+	var hash = crypto.createHash("md5");
+	hash.update(new Buffer("123456", "binary"));
+	var encode = hash.digest('hex');
 
-  connection.query('INSERT INTO user SET ?', {
-    ctime: date.getTime(),
-    mtime: date.getTime(),
-    is_del: 0,
-    password:encode,
-  }, function(err, result) {
-    if (err) throw err;
-    res.json(result.insertId);
-  });
+	connection.query('INSERT INTO user SET ?', {
+		ctime: date.getTime(),
+		mtime: date.getTime(),
+		is_del: 0,
+		password: encode,
+	}, function(err, result) {
+		if (err) throw err;
+		res.json(result.insertId);
+	});
 });
 
 
 
 
-app.get('/deleteUserById', function (req, res) {
-  connection.query('DELETE FROM user WHERE uid = ?', [req.query.uid], function(err, result) {
-    if (err){
-      res.json(false);
-    }else{
-      res.json(true);
-    }
-  });
+app.get('/deleteUserById', function(req, res) {
+	connection.query('DELETE FROM user WHERE uid = ?', [req.query.uid], function(err, result) {
+		if (err) {
+			res.json(false);
+		} else {
+			res.json(true);
+		}
+	});
 });
 
-app.get('/updateUserById', function (req, res) {
-  var q = req.query;
+app.get('/updateUserById', function(req, res) {
+	var q = req.query;
 
-  var r = pinyin(q.uname, {
-    style: pinyin.STYLE_NORMAL,
-    heteronym: true
-  });
+	var r = pinyin(q.uname, {
+		style: pinyin.STYLE_NORMAL,
+		heteronym: true
+	});
 
-  var account = r.map(function(v){
-    return v[0];
-  }).join('');
-  var sindex = account[0];
+	var account = r.map(function(v) {
+		return v[0];
+	}).join('');
+	var sindex = account[0];
 
-  connection.query( 'UPDATE user SET uname = ?, phone = ?, tel = ?, account = ?, sindex = ?  WHERE uid = ?'
-  , [ q.uname.trim(), q.phone.trim(), q.tel.trim(), account, sindex, q.uid] , function(err, results) {
-    if (err){
-      res.json(false);
-    }else{
-      res.json(true);
-    }
-  });
+	connection.query('UPDATE user SET uname = ?, phone = ?, tel = ?, account = ?, sindex = ?  WHERE uid = ?', [q.uname.trim(), q.phone.trim(), q.tel.trim(), account, sindex, q.uid], function(err, results) {
+		if (err) {
+			res.json(false);
+		} else {
+			res.json(true);
+		}
+	});
 });
 
-app.get('/getAllUser', function (req, res) {
-  var columns = ['account','uname', 'phone', 'tel', 'uid','sindex'];
-  connection.query('SELECT ?? from user', [columns], function(err, rows, fields) {
-    if (err) throw err;
-    res.json(rows);
-  });
+app.get('/getAllUser', function(req, res) {
+	var columns = ['account', 'uname', 'phone', 'tel', 'uid', 'sindex'];
+	connection.query('SELECT ?? from user', [columns], function(err, rows, fields) {
+		if (err) throw err;
+		res.jsonp(rows);
+	});
 });
 
-app.get('/getUserById', function (req, res) {
-  var uid = req.query.uid;
-  var columns = ['uname', 'phone', 'tel', 'uid','sindex'];
-  connection.query('SELECT ?? from user where uid = ?',[columns,uid], function(err, rows, fields) {
-    if (err) throw err;
-    res.json(rows[0]);
-  });
+app.get('/getUserById', function(req, res) {
+	var uid = req.query.uid;
+	var columns = ['uname', 'phone', 'tel', 'uid', 'sindex'];
+	connection.query('SELECT ?? from user where uid = ?', [columns, uid], function(err, rows, fields) {
+		if (err) throw err;
+		res.jsonp(rows[0]);
+	});
 });
 
-app.get('/getAuthById', function (req, res) {
-  var uid = req.query.uid;
-  var columns = ['authority'];
-  connection.query('SELECT ?? from user where uid = ?', [columns,uid], function(err, rows, fields) {
-    if (err) throw err;
-    res.json(rows[0].authority);
-  });
+app.get('/getAuthById', function(req, res) {
+	var uid = req.query.uid;
+	var columns = ['authority'];
+	connection.query('SELECT ?? from user where uid = ?', [columns, uid], function(err, rows, fields) {
+		if (err) throw err;
+		res.jsonp(rows[0].authority);
+	});
+
 });
--- static/phone/js/main.js
+++ static/phone/js/main.js
@@ -1,147 +1,185 @@
-$(function(){
-  var contacts = [];
+$(function() {
 
-  //侧边栏
-  var sideEl = $('.indexlist');
+	var contacts = [];
 
-  //用户列表
-  var userlistEl = $('.userlist');
+	//侧边栏
+	var sideEl = $('.indexlist');
 
-  //始终显示的栏目字母提示
-  var fixedIndex = $('.fixedindex');
+	//用户列表
+	var userlistEl = $('.userlist');
 
-  //纪录每一组成员的offsettop
-  var toplist = [];
+	//纪录每一组成员的offsettop
+	var toplist = [];
 
-  //页面滚动条的偏移量
-  var off;
+	//页面滚动条的偏移量
+	var off;
 
-  ///////// 触摸跳转需要的变量
-  // 侧栏中每一个li的高度
-  var sep;
-  // 侧栏距离屏幕顶点的距离
-  var sidetop;
-  // 头部和颈部的高度
-  var soff = $('.header').height() + $('.sub-header').height();
-
-
-  var sync = function(){
-    $.get('/getAllUser').then(function(list){
-      contacts = list;
-      render(contacts);
-      localStorage.__findu__data = JSON.stringify(contacts);
-    },'json');
-  }
-
-  var  render = function(contacts){
-    var data = {};
-    contacts.forEach(function(v){
-      var key = v.sindex.toUpperCase();
-      if( !data[key] ){
-        data[key] = [];
-      }
-      data[key].push(v);
-    })
-    var indexlists = Object.keys(data).sort();
-
-    var userlistHtml = '';
-    var sideElHtml = '';
-    indexlists.forEach(function(v){
-      sideElHtml += '<li>'+v.toUpperCase()+'</li>';
-
-      var arr = data[v].sort(function(a,b){
-        return a.uname > b.uname;
-      });
-      userlistHtml += '<dt>'+v.toUpperCase()+'</dt>'
-      arr.forEach(function(v){
-        userlistHtml += '<dd>'+v.uname+'<a href="tel:'+v.phone+'"></a></dd>';
-      })
-    })
-
-    //创建侧边栏 和 用户列表
-    sideEl.html(sideElHtml);
-    sideEl.height( function(){
-      return $(this).children().eq(0).outerHeight(true) * indexlists.length;
-    } );
-    sideEl.css({
-      top: ($(window).outerHeight(true)-sideEl.height())/2
-    })
-    userlistEl.html(userlistHtml)
-
-    //去掉每组最后一个成员的分割线
-    userlistEl.find('dt').prev().css('border','none');
-
-    toplist = userlistEl.find('dt').map(function(i,v){
-      return {top:$(this).offset().top,index:indexlists[i]} ;
-    }).get();
-    fixedIndex.text(toplist[0].index);
-
-    off =  $('.header').height()
-    + $('.sub-header').height()
-    + fixedIndex.height();
-
-
-    //触摸滚动需要的变量
-    sep = sideEl.find('li').outerHeight(true);
-    sidetop = sideEl.position().top;
-  }
-
-
-  /////////////////////////////////////////////////////////
-  // if(localStorage.__findu__data){
-    // contacts = JSON.parse(localStorage.__findu__data);
-    // render(contacts);
-  // }else{
-  sync();
-  // }
+	///////// 触摸跳转需要的变量
+	// 侧栏中每一个li的高度
+	var sep;
+	// 侧栏距离屏幕顶点的距离
+	var sidetop;
 
 
 
-  ////////////////////////////////////////////////////////////////////
-  //头部固定字母条
+	var sync = function() {
+		$.ajax({
+			url: "/getAlluser",
+			dataType: "jsonp",
+			success: function(list) {
+				contacts = list;
+				render(contacts);
+				localStorage.__findu__data = JSON.stringify(contacts);
+			},
+		})
+	}
 
-  $(window).on('scroll',function() {
-    var s = $(this).scrollTop() + off;
-    if(toplist){
-      toplist.forEach(function(v){
-        if( s >= v.top ){
-          fixedIndex.text(v.index);
-          return;
-        }
-      })
-    }
-  });
+	var render = function(contacts) {
+		var data = {};
+		contacts.forEach(function(v) {
+			var key = v.sindex.toUpperCase();
+			if (!data[key]) {
+				data[key] = [];
+			}
+			data[key].push(v);
+		})
+		var indexlists = Object.keys(data).sort();
 
-  /////////////////////////////////////////////////////////////////////
-  //右侧点击和拖动
-  $('.indexlist').on('touchstart touchmove',function(e){
-    var y = e.originalEvent.changedTouches[0].clientY;
-    var x = Math.floor( (y - sidetop )/sep);
-    if(x < 0 || x > toplist.length - 1 ){
-      return false;
-    }
-    $(window).scrollTop(toplist[x].top - soff);
-    return false;
-  });
+		var userlistHtml = '';
+		var sideElHtml = '';
+		indexlists.forEach(function(v) {
+			sideElHtml += '<li>' + v.toUpperCase() + '</li>';
 
-  /////////////////////////////////////////////////////////////////
-  //搜索功能
-  var search = function(key){
-    var tmp = contacts.filter(function(v){
-      if( v.uname.indexOf(key) !== -1
-      || v.phone.indexOf(key) !== -1
-      || v.tel.indexOf(key) !== -1
-      || v.account.indexOf(key)!== -1){
-        return true;
-      }else{
-        return false;
-      }
-    })
-    render(tmp);
-  }
+			var arr = data[v].sort(function(a, b) {
+				return a.uname > b.uname;
+			});
+			userlistHtml += '<dt>' + v.toUpperCase() + '</dt>'
+			arr.forEach(function(v) {
+				userlistHtml += '<dd>' + v.uname + '<a href="tel:' + v.phone + '"></a></dd>';
+			})
+		})
 
-  $("#search").on('keyup',function(e){
-    search( $(this).val().trim() )
-  })
+		//创建侧边栏 和 用户列表
+		sideEl.html(sideElHtml);
+		sideEl.height(function() {
+			return $(this).children().eq(0).outerHeight(true) * indexlists.length;
+		});
+		sideEl.css({
+			top: ($(window).outerHeight(true) - sideEl.height()) / 2
+		})
+		userlistEl.html(userlistHtml)
+
+		//去掉每组最后一个成员的分割线
+		userlistEl.find('dt').prev().css('border', 'none');
+
+
+
+		if (indexlists.length !== 0) {
+			toplist = userlistEl.find('dt').map(function(i, v) {
+				return {
+					top: $(this).offset().top,
+					index: indexlists[i]
+				};
+			}).get();
+			if ($('.fixedindex').text().trim() === '') {
+				$('.fixedindex').text(toplist[0].index);
+			}
+		} else {
+			$('.fixedindex').text('');
+		}
+
+
+		off = $('.header').height() + $('.sub-header').height() + $('.fixedindex').height();
+
+
+		//触摸滚动需要的变量
+		sep = sideEl.find('li').outerHeight(true);
+		sidetop = sideEl.position().top;
+	}
+
+
+	/////////////////////////////////////////////////////////
+	//保障没有网络也能使用
+	if (localStorage.__findu__data) {
+		contacts = JSON.parse(localStorage.__findu__data);
+		setTimeout(function() {
+			render(contacts);
+		}, 0);
+		setTimeout(function() {
+			sync();
+		}, 500);
+		//校验用户是否   依然  存在
+		//		setTimeout(function() {
+		//			$.ajax({
+		//				url: "http://139.129.167.201:3000/",
+		//				dataType: "jsonp",
+		//				success: function(list) {
+		//					contacts = list;
+		//					render(contacts);
+		//					localStorage.__findu__data = JSON.stringify(contacts);
+		//				},
+		//			})
+		//		})
+	} else {
+		sync();
+	}
+
+	// fixed andorid>>>>>>>>>
+	$(window).on('resize', function() {
+		render(contacts);
+	})
+
+	////////////////////////////////////////////////////////////////////
+	//头部固定字母条
+
+	$(window).on('scroll', function() {
+		var s = $(this).scrollTop() + off;
+		if (toplist) {
+			toplist.forEach(function(v) {
+				if (s >= v.top) {
+					$('.fixedindex').text(v.index);
+					return;
+				}
+			})
+		}
+	});
+
+	/////////////////////////////////////////////////////////////////////
+	//右侧点击和拖动
+	$('.indexlist').on('touchstart touchmove', function(e) {
+		var soff = $('.header').height() + $('.sub-header').height();
+		var y = e.originalEvent.changedTouches[0].clientY;
+		var x = Math.floor((y - sidetop) / sep);
+		if (x < 0 || x > toplist.length - 1) {
+			return false;
+		}
+		$(window).scrollTop(toplist[x].top - soff);
+		return false;
+	});
+
+	/////////////////////////////////////////////////////////////////
+	//搜索功能
+	var search = function(key) {
+		var tmp = contacts.filter(function(v) {
+			if (v.uname.indexOf(key) !== -1 || v.phone.indexOf(key) !== -1 || v.tel.indexOf(key) !== -1 || v.account.indexOf(key) !== -1) {
+				return true;
+			} else {
+				return false;
+			}
+		})
+		render(tmp);
+	}
+
+	// fixed ios position:fixed;
+	$('#search').on('touchstart', function() {
+		$(window).scrollTop(0);
+	})
+
+	$('#search').on('input', function(e) {
+		search($(this).val().trim())
+		if ($(this).val().trim() === '') {
+			$(this).trigger('blur');
+		}
+	})
 
 })
