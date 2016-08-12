01bad89405a28beea947f24f0bd41adf3ce7d1e6
nate-river <mayanlong7@icloud.com>
2016-08-12 19:14:17 +0800

    !!

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ var connection = mysql.createConnection({
 	host: 'localhost',
 	port: '3306',
 	user: 'root',
-	password: 'root',
+	password: '',
 	database: 'uek',
 	// socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
 });
